a59f3e315bd4d7313eaff8ce59cc7593dceef493
Raju-112 <115540555+Raju-112@users.noreply.github.com>
2023-02-06 14:48:55 +0530

    Update script.sh

--- script.sh
+++ script.sh
@@ -2,3 +2,8 @@ git clone https://github.com/Raju-112/Agri.git
 cd Agri
 pip3 install -r requirements.txt
 screen -m -d python app.py
+
+git clone https://github.com/Raju-112/Fuel-Consumption-Rating.git
+cd Fuel-Consumption-Rating
+pip3 install -r requirements.txt
+screen -m -d python app.py
